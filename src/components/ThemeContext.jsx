ca35e50744776fcd969345812fa7d51d550cc777
YashMankar1812 <yashmankar1812>
2024-12-07 17:45:56 +0530

    Fifth commit

--- src/components/ThemeContext.jsx
+++ src/components/ThemeContext.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { createContext, useState, useEffect } from 'react';
 
 export const ThemeContext = createContext();
@@ -22,28 +21,3 @@ export const ThemeProvider = ({ children }) => {
     </ThemeContext.Provider>
   );
 };
-=======
-import React, { createContext, useState, useEffect } from 'react';
-
-export const ThemeContext = createContext();
-
-export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
-
-  useEffect(() => {
-    document.documentElement.classList.remove('light', 'dark');
-    document.documentElement.classList.add(theme);
-    localStorage.setItem('theme', theme);
-  }, [theme]);
-
-  const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
-  };
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
-};
->>>>>>> 06c2902c8872ef915a2ecf686b77835a5dfb7f4e
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'class', 
@@ -18,24 +17,3 @@ export default {
   plugins: [],
 }
 
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  darkMode: 'class', 
-  build :{
-    rollupOptions: {
-      external: ['react-icons']
-    }
-  },
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-
-  plugins: [],
-}
-
->>>>>>> 06c2902c8872ef915a2ecf686b77835a5dfb7f4e
